cddde0333d3da42dff0a63fe9f0d536127c47882
sebas0505r <sebria26@bergen.org>
2025-10-28 13:53:20 -0400

    completed marshmellow - 10/28/25

---
+++ monster.java
@@ -0,0 +1,38 @@
+public class monster {
+    private int arms, eyes, buttons;
+    private String name;
+
+    public monster() {
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setArms(int arms) {
+        this.arms = arms;
+    }
+    
+    public void setEyes(int eyes) {
+        this.eyes = eyes;
+    }
+
+    public void setButtons(int buttons) {
+        this.buttons = buttons;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getArms() {
+        return arms;
+    }
+
+    public int getEyes() {
+        return eyes;
+    }
+    public int getButtons() {
+        return buttons;
+    }
+}
