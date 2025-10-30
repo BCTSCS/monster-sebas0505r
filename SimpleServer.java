9fa0335310657207a9234077155ba54cfefab26b
sebas0505r <sebria26@bergen.org>
2025-10-30 14:09:38 -0400

    done completed 10/30/25

---
+++ SimpleServer.java
@@ -0,0 +1,32 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public class SimpleServer {
+    private ServerSocket serverSocket;
+    private Socket socket;
+    private PrintWriter out;
+    private Scanner in;
+    public SimpleServer(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+        System.out.println("Server started and listening: " + port);
+    }
+    public void acceptClient() {
+    }
+    public String receiveMessage() {
+        return "";
+    }
+    public void sendMessage(String message) {
+    }
+    public void close() {
+    }
+    public static void main(String[] args) {
+        try{
+            SimpleServer s= new SimpleServer(8888);
+    }
+        catch(Exception e){
+         System.out.println("Error occurred");
+            e.printStackTrace();
+    }
+}
+}
